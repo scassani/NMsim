a30cd0c18d6358e0bb7f38e861f2c3efdaf71d70
Philip Delff <philip@delff.dk>
2024-12-08 12:22:36 -0500

    supporting controls streams with no existing sizes

--- R/NMupdateSizes.R
+++ R/NMupdateSizes.R
@@ -53,22 +53,21 @@ NMupdateSizes <- function(file.mod,newfile=file.mod,wipe=FALSE,...){
 
 NMreadSizes <- function(file.mod){
     lines.sizes <- NMreadSection(file=file.mod,section="SIZES",keep.empty=F,keep.comments=FALSE,keep.name=FALSE)
+    if(is.null(lines.sizes)) return(NULL)
     
-    if(!is.null(lines.sizes)){
-        ## make it just one line
-        lines.sizes <- paste(lines.sizes,collapse=" ")
-        ## Replace all " +" by " " (multiple spaces to just one space)
-        ## lines.sizes <- gsub(" +"," ",lines.sizes)
-        ## clean the found SIZES first to make sure " *=" and "= *" are replaced by "=".
-        ## lines.sizes <- gsub(" *= *","=",lines.sizes)
-        lines.sizes <- NMdata:::cleanSpaces(lines.sizes)
-        ## Then strsplit with " " as separator should identify parameters and values.
-        strings.sizes <- strsplit(lines.sizes,split=" ")[[1]]
-        ## We now have strings "par1=val1" and "par2=val2". These have to be separated - could be done by strsplit again, this time using = as separator.
-        list.sizes.strings <- lapply(strings.sizes,function(x)strsplit(x,split="=")[[1]])
-        list.sizes.lists <- lapply(list.sizes.strings,function(x)setNames(list(x[2]),x[1]))
-        list.sizes <- do.call(c,list.sizes.lists)
-    }
+    ## make it just one line
+    lines.sizes <- paste(lines.sizes,collapse=" ")
+    ## Replace all " +" by " " (multiple spaces to just one space)
+    ## lines.sizes <- gsub(" +"," ",lines.sizes)
+    ## clean the found SIZES first to make sure " *=" and "= *" are replaced by "=".
+    ## lines.sizes <- gsub(" *= *","=",lines.sizes)
+    lines.sizes <- NMdata:::cleanSpaces(lines.sizes)
+    ## Then strsplit with " " as separator should identify parameters and values.
+    strings.sizes <- strsplit(lines.sizes,split=" ")[[1]]
+    ## We now have strings "par1=val1" and "par2=val2". These have to be separated - could be done by strsplit again, this time using = as separator.
+    list.sizes.strings <- lapply(strings.sizes,function(x)strsplit(x,split="=")[[1]])
+    list.sizes.lists <- lapply(list.sizes.strings,function(x)setNames(list(x[2]),x[1]))
+    list.sizes <- do.call(c,list.sizes.lists)
 
     list.sizes
 }

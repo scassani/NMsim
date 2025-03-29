d79e87243350667805851b829070228338fb71bb
Philip Delff <pdelff@bu.edu>
2025-03-29 14:05:02 -0400

    manual tests on win

--- devel/NMsim_test_win.R
+++ devel/NMsim_test_win.R
@@ -6,7 +6,7 @@ NMdataConf(as.fun="data.table")
 library(data.table)
 
 dt.dos <- NMcreateDoses(AMT=300,TIME=0)
-dt.sim <- addEVID2(doses=dt.dos,time.sim=c(1,6,12),CMT=2)
+dt.sim <- addEVID2(data=dt.dos,TIME=c(1,6,12),CMT=2)
 dt.sim[,BBW:=40][,ROW:=.I]
 
 dt.sim.known <- egdt(dt.sim[,!("ID")],data.table(ID=101:105))
@@ -24,39 +24,37 @@ file.mod <- "../tests/testthat/testData/nonmem/xgxr021.mod"
 
 
 set.seed(43)
-## Not specifying method.execute - PSN should not be used if NMdataConf was used to specify Nonmem path. 
+## Not specifying method.execute - PSN should not be used if
+## NMdataConf was used to specify Nonmem path.
 simres <- NMsim(file.mod,
                 data=dt.sim,
-                text.table="PRED IPRED",
+                table.vars="PRED IPRED",
                 dir.sims="testOutput",
                 name.sim="default_01"
-                ,method.update.inits="nmsim"
                 )
 
 ## use PSN (path to folder containing "execute" executable may need to be specified above.
 simres <- NMsim(file.mod,
                 data=dt.sim,
-                text.table="PRED IPRED",
+                table.vars="PRED IPRED",
                 dir.sims="testOutput",
                 name.sim="default_01"
-                ,method.update.inits="nmsim"
                ,method.execute="psn"
                 )
 
 ## Explicitly ask to use Nonmem directly
 simres <- NMsim(file.mod,
                 data=dt.sim,
-                text.table="PRED IPRED",
+                table.vars="PRED IPRED",
                 dir.sims="testOutput",
                 name.sim="default_01"
-                ,method.update.inits="nmsim"
                ,method.execute="NMsim"
                 )
 
 ## not specifying method.update.inits - default is using PSN if found
 simres <- NMsim(file.mod,
                 data=dt.sim,
-                text.table="PRED IPRED",
+                table.vars="PRED IPRED",
                 dir.sims="testOutput",
                 name.sim="default_01"
                 )

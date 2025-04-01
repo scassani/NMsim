0e69eb5f852d526d8249cac82653156bcefbe72b
Philip Delff <philip@delff.dk>
2025-04-01 13:20:26 -0400

    debug

--- R/NMrunWin.R
+++ R/NMrunWin.R
@@ -105,6 +105,7 @@ NMrunWin <- function(fn.mod,dir.mod.abs,exts.cp,meta.tables,path.nonmem,clean,fu
 ### Section end: Dummy variables, only not to get NOTE's in pacakge checks
 
     cp.if.pres <- function(fn,dest){
+        print(sprintf("IF EXIST \"%s\" COPY /Y \"%s\" \"%s\" >nul",fn,fn,dest))
         sprintf("IF EXIST \"%s\" COPY /Y \"%s\" \"%s\" >nul",fn,fn,dest)
     }
     rm.if.pres <- function(fn){

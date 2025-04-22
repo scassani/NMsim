b2b9a5fd3bd4d6d8b073c0c3db05c493c4c62383
Philip Delff <philip@delff.dk>
2025-04-22 14:27:14 -0400

    modifyModel on ctls

--- R/modifyModel.R
+++ R/modifyModel.R
@@ -1,38 +1,53 @@
-##' Internal method for handling modify.model argument to NMsim
-##' @param modify.model A list
+##' Internal method for handling modify argument to NMsim
+##' @param modify A list
 ##' @param dt.models a data.table
+##' @param list.ctl List of coontrol streams as lines
+##' @return dt.models (data.table) or result list.ctl (list)
+##' @import data.table
 ##' @keywords internal
 
 
 
-modifyModel <- function(modify.model,dt.models){
+modifyModel <- function(modify,dt.models=NULL,list.ctl=NULL){
 
     ##
-    modifyModelOne <- function(modify.model,dt.models){
+    modifyModelOne <- function(modify,dt.models=dt.models,list.ctl=list.ctl){
         . <- NULL
         path.sim <- NULL
         ROWMODEL <- NULL
         
         ## grab name and other arguments for NMwriteSection. 
-        if(is.list(modify.model[[1]])){
-            args <- modify.model[[1]]
-            args$section <- toupper(names(modify.model))
+        if(is.list(modify[[1]])){
+            args <- modify[[1]]
+            args$section <- toupper(names(modify))
             args$quiet <- TRUE
             args$backup <- FALSE
             ## do not use list.sections
         } else {
-            args <- list(quiet=TRUE,backup=FALSE,list.sections=modify.model)
+            args <- list(quiet=TRUE,backup=FALSE,list.sections=modify)
         }
-        dt.models[,{
-            args.all= append(list(files=path.sim),args)
-            do.call(NMwriteSection,args.all)
-        },by=.(ROWMODEL)]
-
+        if(!is.null(list.ctl)){
+            result <- lapply(list.ctl,function(ctl){
+                args.all= append(list(lines=ctl),args)
+                do.call(NMdata:::NMwriteSectionOne,args.all)
+            })
+        }
+        if(!is.null(dt.models)){
+            dt.models[,{
+                args.all= append(list(files=path.sim),args)
+                do.call(NMwriteSection,args.all)
+            },by=.(ROWMODEL)]
+            result <- dt.models
+        }
+        result
     }
 
-    for(Nmodif in 1:length(modify.model)){
-        modifyModelOne(modify.model[Nmodif],dt.models)
+    ## for(Nmodif in 1:length(modify)){
+    ##     modifyModelOne(modify[Nmodif],dt.models)
+    ## }
+    for(Nmodif in 1:length(modify)){
+        list.ctl <- modifyModelOne(modify[Nmodif],dt.models=dt.models,list.ctl=list.ctl)
     }
-
-    dt.models
+    
+ list.ctl
 }
